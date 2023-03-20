54c642651e2407be4c94953b53d8300d6606d0c1
maratdev <voredev@gmail.com>
2023-03-20 17:28:29 +0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() {return (
+<div className="wrapper">
+  <header className="header">
+    <div className="header__logo">
+      <img className="header__logo-img" src="/images/logo.svg" alt="лого"/>
+      <div className="header__wrap">
+        <h3 className="header__title">REACT SNEAKERS</h3>
+        <p className="header__subtitle">Магазин лучших кроссовок</p>
+      </div>
     </div>
+
+    <ul className="header__basket">
+      <li className="header__item"><img className="header__item-img" src="/images/basket.svg" alt=""/><span className="header__price">1234 руб.</span></li>
+      <li className="header__item"><img src="/images/heart.svg" alt=""/></li>
+      <li className="header__item"><img src="/images/user.svg" alt=""/></li>
+    </ul>
+  </header>
+  <main className="content">
+    <section className="slider"></section>
+    <section className="card-grid">
+      <h1>Все кроссовки</h1>
+    </section>
+  </main>
+</div>
+
   );
 }
 
