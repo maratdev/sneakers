5eddea71410e0ec2a6fb40f5ad64cb6bccce73f6
maratdev <voredev@gmail.com>
2023-04-07 00:28:17 +0300

    feat(drawer) добавление блока корзины

--- src/App.js
+++ src/App.js
@@ -5,6 +5,29 @@ import './index.scss';
 
 function App() {return (
 <div className="wrapper">
+  <div className="drawer"> 
+    <div className="drawer__container">
+      <h3 className="drawer__title">Корзина</h3>
+      <ul className="cart">
+        <li className="cart__item">
+          <a className="cart__src" href=""><img className="cart__img" src="./images/card/2.png" alt="Мужские Кроссовки Nike Air Max 270"/></a>
+          <div className="cart__desc">
+            <h4 className="cart__title"><a className="cart__title-src" href="">Мужские Кроссовки Nike Air Max 270</a></h4>
+            <h5 className="cart__price">12 999 руб.</h5>
+          </div>
+          <button aria-label="Удалить" type="button" className="cart__del"></button>
+        </li>
+        <li className="cart__item">
+          <a className="cart__src" href=""><img className="cart__img" src="./images/card/1.png" alt="Мужские Кроссовки Nike Air Max 270"/></a>
+          <div className="cart__desc">
+            <h4 className="cart__title"><a className="cart__title-src" href="">Мужские Кроссовки Nike Blazer Mid Suede</a></h4>
+            <h5 className="cart__price">3 999 руб.</h5>
+          </div>
+          <button aria-label="Удалить" type="button" className="cart__del"></button>
+        </li>
+      </ul>
+    </div>
+  </div>
   <header className="header">
     <div className="header__logo">
       <img className="header__logo-img" src="/images/logo.svg" alt="лого"/>
@@ -13,7 +36,6 @@ function App() {return (
         <p className="header__subtitle">Магазин лучших кроссовок</p>
       </div>
     </div>
-
     <ul className="header__basket">
       <li className="header__item"><img className="header__item-img" src="/images/basket.svg" alt=""/><span className="header__price">1234 руб.</span></li>
       <li className="header__item"><img src="/images/heart.svg" alt=""/></li>
@@ -37,7 +59,7 @@ function App() {return (
         <li className="grid__items">
           <article className="card">
             <button className="card__like" type="button" aria-label="Лайк"></button>
-            <a href=""><img className="card__img" src="./images/card/1.png" alt="Мужские Кроссовки Nike Blazer Mid Suede"/></a>
+            <a  href=""><img className="card__img" src="./images/card/1.png" alt="Мужские Кроссовки Nike Blazer Mid Suede"/></a>
             <h3 className="card__title"><a className="card__title-src" href="">Мужские Кроссовки Nike Blazer Mid Suede</a></h3>
             <div className="card__desc">
               <p className="card__cost">Цена</p>
